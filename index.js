8226102067fb21f882391eca92673136dfe1d342
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-14 21:40:07 +0600

    router ongoing

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+
+
 dotenv.config();
 
 connectDB();
@@ -14,15 +16,9 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 }
 );
-app.get("/api/products", (req, res) => {
-    res.json(products);
-}
-);
-app.get("/api/products/:id", (req, res) => {
-    const product = products.find(p => p._id === req.params.id);
-    res.json(product);
-}
-);
+
+app.use("/api/products", productRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
