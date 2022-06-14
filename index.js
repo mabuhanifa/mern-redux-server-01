4b9131436c90e8e2e38886b42ef500f620366036
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-14 15:24:11 +0600

    dotenv

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const products = require("./data/products");
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 app.use(cors());
