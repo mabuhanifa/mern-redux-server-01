2f3ac70fcc0d96cffe6e1a58b0bbc304cced46c5
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-14 16:48:43 +0600

    mongoosedone

--- models/orderModel.js
+++ models/orderModel.js
@@ -15,17 +15,97 @@ const orderSchema =  mongoose.Schema({
             type: Number,
             required: true,
         },
-        
+        image : {
+            type: String,
+            required: true,
+        },
+        price : {
+            type: Number,
+            required: true,
+        },
+        product : {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+            required: true,
+        }
+
+
     }],
-    totalPrice: {
-        type: Number,
+    shippingAddress : {
+        address : {
+            type: String,
+            required: true,
+        },
+        city : {
+            type: String,
+            required: true,
+        },
+        postalCode : {
+            type: String,
+            required: true,
+        },
+        country : {
+            type: String,
+            required: true,
+
+        },
+
+},
+    paymentMethod : {
+        type : String,
         required: true,
-        default: 0,
-    }
+    },
+    paymentResult : {
+        id : {
+            type: String
+        },
+        status : {
+            type: String
+        },
+        update_time : {
+            type: String,
+        },
+        email_address : {
+            type: String
+        }},
+
+        taxPrice : {
+            type: Number,
+            required: true,
+            default: 0.0,
+        },
+        shippingPrice : {
+            type: Number,
+            required: true,
+            default: 0.0,
+        },
+        totalPrice : {
+            type: Number,
+            required: true,
+            default: 0.0,
+        },
+        isPaid : {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+        paidAt : {
+            type: Date,
+        },
+        deliveredAt : {
+            type: Date,
+        }
+
 }
 , {
     timestamps: true,
 }
+
+
+
+
+
+
 );
 const Order = mongoose.model("Order", orderSchema);
 
