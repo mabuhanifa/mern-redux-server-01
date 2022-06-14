b13a2574fbecc9c7a5375a1757a8e704d1ad85cf
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-14 17:06:31 +0600

    mongoosefinished

--- models/userModel.js
+++ models/userModel.js
@@ -1,33 +1,31 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        unique : true,
-        required: true,
+      type: String,
+      unique: true,
+      required: true,
     },
     password: {
-        type: String,
-        required: true,
-        minlength: 6,
-        select: false,
+      type: String,
+      required: true,
     },
-    isAdmin : {
-        type: Boolean,
-        required : true,
-        default: false,
-    }
-}
-, {
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
     timestamps: true,
-}
+  }
 );
 
 const User = mongoose.model("User", userSchema);
 
 export default User;
-
