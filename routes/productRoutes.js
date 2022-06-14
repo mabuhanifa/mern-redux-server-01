be7221b213aaf53075c335211ad53e0600eeabcb
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-15 00:00:45 +0600

    error handling done

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -18,11 +18,14 @@ router.get("/", asyncHandler( async (req, res) => {
 
 router.get("/:id", asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
+
+
     if(product){
         res.json(product);
     }
     else{
-        res.status(404).json({message: "Product not found"});
+        res.status(404);
+        throw new Error("Product not found");
     }
 }
 ));
