1dee28c6ec1c10a1d871cab152694260a1e6f2b1
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-16 01:13:17 +0600

    user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,30 +10,35 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email: email });
 
   if (user && (await user.matchPassword(password))) {
-     
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: generateToken(user._id)
+      token: generateToken(user._id),
     });
+  } else {
+    res.status(401);
+    throw new Error("Invalid credentials");
   }
-    else {
-        res.status(401);
-        throw new Error("Invalid credentials");
-        }
-        
 });
-
-
 
 // @desc   Get  user profile
 // @route  GET /api/v1/users
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  
-  res.send('hi');
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("Invalid credentials");
+  }
 });
 export { authUser, getUserProfile };
 
