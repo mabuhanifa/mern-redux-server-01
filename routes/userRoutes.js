b13cb27e85fa0e27c80efaf517ceaae98dd0e33e
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-25 14:40:04 +0600

    orderController added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,8 @@ import express from "express";
 import {
     authUser,
     getUserProfile,
-    registerUser
+    registerUser,
+    updateUserProfile
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
@@ -12,6 +13,9 @@ const router = express.Router();
 // @access Public
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
