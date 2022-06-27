bd1d197e9b5e0f7c4b49a512e6567c600820aa0a
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-27 12:56:46 +0600

    order pay constants added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -18,7 +18,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 
   if (orderItems && orderItems.length === 0) {
     res.status(400);
-    throw new Error(`No order Items`);
+    throw new Error("No order items");
     return;
   } else {
     const order = new Order({
@@ -31,7 +31,9 @@ const addOrderItems = asyncHandler(async (req, res) => {
       shippingPrice,
       totalPrice,
     });
+
     const createdOrder = await order.save();
+
     res.status(201).json(createdOrder);
   }
 });
