abdb6bd85035e1b507f839772c067faf4fa7bc78
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-29 17:00:45 +0600

    delete user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -106,5 +106,25 @@ const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find({});
   res.json(users);
 });
-export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
+
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    await user.remove();
+    res.json({ message: "User removed" });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+};
 
