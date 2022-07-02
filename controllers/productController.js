97ce97fec1cfc58971b3d17458bf0ac474c77ee5
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-02 14:43:40 +0600

    creatProduct

--- controllers/productController.js
+++ controllers/productController.js
@@ -14,5 +14,34 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product removed" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+export { getProducts, getProductById, deleteProduct, createProduct };
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,19 +1,13 @@
 import express from "express";
 import {
+    deleteProduct,
     getProductById,
     getProducts
 } from "../controllers/productController.js";
+import { admin, protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
-// @desc   Get all products
-// @route  GET /api/v1/products
-// @access Public
 router.route("/").get(getProducts);
-
-// @desc   Get single product
-// @route  GET /api/v1/products/:id
-// @access Public
-
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
