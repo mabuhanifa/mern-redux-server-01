f1919fe33294cf68142c287e216834eedde5c949
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-04 01:35:18 +0600

    top rated route

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,25 @@
 import asyncHandler from "express-async-handler";
 import Product from "../models/productModel.js";
+
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
+  const pageSize = 10;
+  const page = Number(req.query.pageNumber) || 1;
+
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
@@ -66,11 +83,18 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.json(products);
+});
+
 export {
   getProducts,
   getProductById,
   deleteProduct,
   createProduct,
   updateProduct,
+  getTopProducts,
 };
 
