2091733ab29066ff2c22e71c7b11067a557924c4
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-03 16:34:07 +0600

    order routes added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -83,5 +83,16 @@ const getMyOrders = asyncHandler(async (req, res) => {
   res.json(orders);
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate("user", "id name");
+  res.json(orders);
+});
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+};
 
